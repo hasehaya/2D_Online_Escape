26984c500724748cce633d322f776df194d43cf8
runcona8@gmail.com <runcona8@gmail.com>
2026-04-30 06:46:40 +0900

    Refactor inventory system; introduce InventoryItem class for better item representation and management

---
+++ Assets/Scripts/InventorySlotUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// インベントリスロットのクリックと選択表示を担当する。
+/// </summary>
+public class InventorySlotUI : MonoBehaviour, IPointerClickHandler
+{
+    private InventorySlot _slot;
+    private Action<InventorySlot> _onClicked;
+    private Image _selectedImage;
+
+    public InventorySlot Slot => _slot;
+
+    public void Initialize(InventorySlot slot, Image selectedImage, Action<InventorySlot> onClicked)
+    {
+        _slot = slot;
+        _selectedImage = selectedImage;
+        _onClicked = onClicked;
+    }
+
+    public void SetSelected(bool isSelected)
+    {
+        if (_selectedImage != null)
+        {
+            _selectedImage.enabled = isSelected;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _onClicked?.Invoke(_slot);
+    }
+}
+
