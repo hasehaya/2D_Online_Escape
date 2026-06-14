b577a4e29e99e08910cdee0497a33546f39f1f0e
runcona8@gmail.com <runcona8@gmail.com>
2026-06-15 07:26:02 +0900

    Add BookShelfObject to implement sliding functionality; enhance editor support for interactive testing

---
+++ Assets/Editor/BookShelfObjectEditor.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(BookShelfObject))]
+    public class BookShelfObjectEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            EditorGUILayout.Space();
+
+            BookShelfObject bookShelfObject = (BookShelfObject)target;
+
+            using (new EditorGUI.DisabledScope(!Application.isPlaying || bookShelfObject.IsSlid))
+            {
+                if (GUILayout.Button("Slide Left"))
+                {
+                    bookShelfObject.RequestSlide();
+                }
+            }
+
+            if (!Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox("Enter Play Mode to test the bookshelf slide.", MessageType.Info);
+            }
+            else if (bookShelfObject.IsSlid)
+            {
+                EditorGUILayout.HelpBox("This bookshelf has already moved.", MessageType.Info);
+            }
+        }
+    }
+}
